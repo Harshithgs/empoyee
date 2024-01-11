b031b2e08e813e3a4352e2442263dc5bd93b0fab
Harshithgs <harshit.shivamoggi@gmail.com>
2024-01-11 12:05:27 +0530

    1st

---
+++ Employeemangmentsystem/src/main/java/com/Planotech/Employeemangmentsystem/contoroller/commoncontroller.java
@@ -0,0 +1,30 @@
+package com.Planotech.Employeemangmentsystem.contoroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
+@Controller
+public class commoncontroller {
+	@GetMapping("/")
+	public String loadHome() {
+		return "homepage";
+	}
+	@GetMapping("/about-us")
+	public String loadAboutUs() {
+		return "AboutUs";
+	}
+
+	@GetMapping("/login")
+	public String loadLogin() {
+		return "login.html";
+	}
+
+	
+	}
+
+
+
